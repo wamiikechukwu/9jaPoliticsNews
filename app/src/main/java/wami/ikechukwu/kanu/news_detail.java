14bcb6749641e978f57cb4abf61d1651afec3703
Wami ikechukwu <wamiikechukwu@gmail.com>
2019-09-18 15:45:35 +0100

    Finally added the onItemClickListener -DONE

--- app/src/main/java/wami/ikechukwu/kanu/news_detail.java
+++ app/src/main/java/wami/ikechukwu/kanu/news_detail.java
@@ -2,11 +2,9 @@ package wami.ikechukwu.kanu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class news_detail extends AppCompatActivity {
 
-    TextView newsdetail_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
