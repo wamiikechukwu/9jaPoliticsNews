5aeb3c121a1344bea515b6358333a6def95960b9
wamiikechukwu <wamiikechukwu@gmail.com>
2020-07-27 16:19:46 +0100

    created a new barnch for development

--- app/src/main/java/wami/ikechukwu/kanu/MainActivity.java
+++ app/src/main/java/wami/ikechukwu/kanu/MainActivity.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
---
+++ app/src/main/java/wami/ikechukwu/kanu/OnboardingActivity.kt
@@ -0,0 +1,11 @@
+package wami.ikechukwu.kanu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
