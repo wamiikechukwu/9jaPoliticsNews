ec0cc4eda791234e7e38dac7932f31c6b9f6eba2
wamiikechukwu <wamiikechukwu@gmail.com>
2020-07-30 20:29:10 +0100

    Successful added the ViewModel

--- app/src/main/java/wami/ikechukwu/kanu/view/ui/OnboardingActivity.kt
+++ app/src/main/java/wami/ikechukwu/kanu/view/ui/OnboardingActivity.kt
@@ -3,17 +3,27 @@ package wami.ikechukwu.kanu.view.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import wami.ikechukwu.kanu.R
 import wami.ikechukwu.kanu.databinding.ActivityOnboardingBinding
+import wami.ikechukwu.kanu.viewmodel.OnboardingViewModel
 
 class OnboardingActivity : AppCompatActivity() {
+
+    //    FOR THE DATA BINDING
     private lateinit var binding: ActivityOnboardingBinding
+
+    //    FOR THE VIEW MODEL
+    private lateinit var onboardViewModel: OnboardingViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
+
+        onboardViewModel = ViewModelProvider(this)[OnboardingViewModel::class.java]
 
 
     }
