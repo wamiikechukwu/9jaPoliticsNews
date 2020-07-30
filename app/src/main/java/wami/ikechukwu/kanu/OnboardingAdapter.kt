ae6b07c04ff6e94e2382a1df9ceeb9b61c9ef6e5
wamiikechukwu <wamiikechukwu@gmail.com>
2020-07-30 08:17:26 +0100

    Encountered some difficulty on the OnboardingAdapter.kt

--- app/src/main/java/wami/ikechukwu/kanu/OnboardingAdapter.kt
+++ app/src/main/java/wami/ikechukwu/kanu/OnboardingAdapter.kt
@@ -1,3 +1,26 @@
 package wami.ikechukwu.kanu
 
-class OnboardingAdapter
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import wami.ikechukwu.kanu.model.OnboardingModel
+
+class OnboardingAdapter(private val onboardingArrayList: ArrayList<OnboardingModel>) : RecyclerView.Adapter<OnboardingAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnboardingAdapter.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+        return onboardingArrayList.size
+    }
+
+    override fun onBindViewHolder(holder: OnboardingAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+
