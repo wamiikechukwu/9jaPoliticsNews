ea5ac17eb3240187f529772a5b1f4f4a0b2413bb
wamiikechukwu <wamiikechukwu@gmail.com>
2020-08-01 00:39:01 +0100

    optimize the OnboardingActivity.kt

--- app/src/main/java/wami/ikechukwu/kanu/view/ui/OnboardingActivity.kt
+++ app/src/main/java/wami/ikechukwu/kanu/view/ui/OnboardingActivity.kt
@@ -1,18 +1,17 @@
 package wami.ikechukwu.kanu.view.ui
 
-//import wami.ikechukwu.kanu.databinding.ActivityOnboardingBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.viewpager.widget.ViewPager
-import androidx.viewpager2.widget.ViewPager2
 import wami.ikechukwu.kanu.R
+import wami.ikechukwu.kanu.databinding.ActivityOnboardingBinding
 import wami.ikechukwu.kanu.viewmodel.OnboardingViewModel
 
 class OnboardingActivity : AppCompatActivity() {
 
-    //    FOR THE DATA BINDING
-//    private lateinit var binding: ActivityOnboardingBinding
+    //        FOR THE DATA BINDING
+    private lateinit var binding: ActivityOnboardingBinding
 
     //    FOR THE VIEW MODEL
     private lateinit var onboardViewModel: OnboardingViewModel
@@ -21,43 +20,26 @@ class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+//      HIDDING THE ACTION BAR
+        supportActionBar?.hide()
 
-        setContentView(R.layout.activity_onboarding)
+//        BINDING THE LAYOUT TO DATABINDING
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
 
-//        binding = DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
-
+//        ACCESSING THE VIEW MODEL CLASS
         onboardViewModel = ViewModelProvider(this)[OnboardingViewModel::class.java]
 
+//        CALLING THE FUNCTION IN THE VIEW MODEL CLASS
         onboardViewModel.initTheDataModel()
 
+//        CALL A METHOD IN THE VIEW MODEL CLASS
         onboardViewModel.initTheAdapter()
 
-
-        val ik: ViewPager = findViewById(R.id.onboarding_viewpager2)
-        ik.adapter = onboardViewModel.initTheAdapter()
-        ik.setPadding(40, 0, 40, 0)
-
-        // val marginPageTransformer = MarginPageTransformer(50)
-
-//        ik.setPageTransformer(CompositePageTransformer().also {
-//
-//            //it.addTransformer(marginPageTransformer)
-//
-//          it.addTransformer(transformer)
-//        })
+//        VIEWPAGER WIDGET FROM THE LAYOUT
+        binding.onboardingViewpager.adapter = onboardViewModel.initTheAdapter()
+        binding.onboardingViewpager.setPadding(40, 0, 40, 0)
 
     }
-
-
-    val transformer = ViewPager2.PageTransformer { page, position ->
-        page.apply {
-//          translationX = Math.abs(position) * 100f
-//           scaleX = 1f
-//           scaleY
-        }
-    }
-
-
 }
 
 
