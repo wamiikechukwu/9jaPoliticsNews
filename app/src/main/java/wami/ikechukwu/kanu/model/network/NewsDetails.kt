8f91bd5c9af582fa8a26818752d30b522e3229bf
wamiikechukwu <wamiikechukwu@gmail.com>
2020-12-10 14:02:40 +0100

    Remove unnecessary codes and modified the data class model

--- app/src/main/java/wami/ikechukwu/kanu/model/network/NewsDetails.kt
+++ app/src/main/java/wami/ikechukwu/kanu/model/network/NewsDetails.kt
@@ -2,11 +2,10 @@ package wami.ikechukwu.kanu.model.network
 
 
 data class NewsDetails(
-//        val author: String, // Oboh
-//        val content: String, // By Dapo AkinrefonTHE United Niger Delta Energy [+4004 chars]
-//        val description: String, // THE United Niger Delta Energy Development Security Strategy
-//        val publishedAt: String, // 2020-08-10T18:25:57Z
-        val title: String, // NDDC probe: Enough’s enough, UNDEDSS tells FG
-//        val url: String, // https://www.vanguardngr.com/2020/08/nddc-probe-enoughs-enough-undedss-tells-fg/
-        val urlToImage: String // https://i2.wp.com/www.vanguardngr.com/wp-content/uploads/2019/09/NDDC.jpg?fit=1500%2C840&ssl=1
+        val title: String,
+        val description: String,
+        val url: String,
+        val urlToImage: String,
+        val publishedAt: String,
+        val author: String
 )
